458ac8ad8602fe767f829a61e666bcd26b19eb02
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-12 21:28:35 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
   def clickbait
     ["Won't Believe", "Secret", "Top [number]", "Guess"].any? do |e|
       puts title
-      title.include?(e) if title
+      !title.include?(e) if title
     end
   end
 end
